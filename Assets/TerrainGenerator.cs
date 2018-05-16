250b9f29730eb0cc14f81dc0290e638288a0bddb
Nick Jensen <nickrjensen@gmail.com>
2018-05-16 13:19:25 +0200

    setup for part 2

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -1,64 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class TerrainGenerator : MonoBehaviour {
+public class TerrainGenerator : MonoBehaviour
+{
+    Mesh mesh = null;
+    Vector3[] curve = new Vector3[4];
+    List<Vector3> vertices = new List<Vector3> ();
+    List<int> triangles = new List<int> ();
 
-  private Mesh          mesh      = null;
-  private Vector3[]     points    = null;
-  private List<Vector3> vertices  = new List<Vector3> ();
-  private List<int>     triangles = new List<int> ();
+    void Start ()
+    {
+        MeshFilter filter = GetComponent<MeshFilter> ();
+        mesh = filter.mesh;
+        mesh.Clear ();
 
-  void Start () {
+        var xPos = 0f;
+        for (int i = 0; i < curve.Length; i++) {
+            curve [i] = new Vector3 (xPos, Random.Range (1f, 2f), 0f);
+            xPos += 0.5f;
+        }
 
-    MeshFilter filter = GetComponent<MeshFilter>();
-    mesh = filter.mesh;
-    mesh.Clear();
+        int resolution = 20;
+        for (int i = 0; i < resolution; i++) {
+            float t = (float)i / (float)(resolution - 1);
+            Vector3 p = CalculateBezierPoint (t, curve [0], curve [1], curve [2], curve [3]);
+            AddTerrainPoint (p);
+        }
 
-    points = new Vector3[4];
-    for (int i = 0; i < points.Length; i++) {
-      points[i] = new Vector3(0.5f * (float)i, Random.Range(1f, 2f), 0f);
+        mesh.vertices = vertices.ToArray ();
+        mesh.triangles = triangles.ToArray ();
     }
 
-    int resolution = 20;
-    for (int i = 0; i < resolution; i++) {
-      float t = (float)i / (float)(resolution - 1);
-      Vector3 p = CalculateBezierPoint(t, points[0], points[1], points[2], points[3]);
-      AddTerrainPoint(p);
+    void AddTerrainPoint (Vector3 point)
+    {
+        vertices.Add (new Vector3 (point.x, 0f, 0f));
+        vertices.Add (point);
+
+        if (vertices.Count >= 4) {
+            int start = vertices.Count - 4;
+            triangles.Add (start + 0);
+            triangles.Add (start + 1);
+            triangles.Add (start + 2);
+            triangles.Add (start + 1);
+            triangles.Add (start + 3);
+            triangles.Add (start + 2);
+        }
     }
 
-    mesh.vertices = vertices.ToArray();
-    mesh.triangles = triangles.ToArray();
-  }
+    private Vector3 CalculateBezierPoint (float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
 
-  void AddTerrainPoint(Vector3 point) {
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
 
-    vertices.Add(new Vector3(point.x, 0f, 0f));
-    vertices.Add(point);
-
-    if (vertices.Count >= 4) {
-      int start = vertices.Count - 4;
-      triangles.Add(start + 0);
-      triangles.Add(start + 1);
-      triangles.Add(start + 2);
-      triangles.Add(start + 1);
-      triangles.Add(start + 3);
-      triangles.Add(start + 2);
+        return p;
     }
-  }
-
-  private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) {
-
-    float u = 1 - t;
-    float tt = t * t;
-    float uu = u * u;
-    float uuu = uu * u;
-    float ttt = tt * t;
-
-    Vector3 p = uuu * p0;
-    p += 3 * uu * t * p1;
-    p += 3 * u * tt * p2;
-    p += ttt * p3;
-
-    return p;
-  }
 }
